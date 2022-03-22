e199da7e6aa5d28b0b63a159297ca1d67db6b1e1
Mikael Quadros <mikael.quadros@gmail.com>
2022-03-21 21:48:05 -0300

    Tabelas iniciais

---
+++ backend/database/migrations/2022_03_22_000852_create_usuarios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string("nome");
+            $table->string("email")->unique();
+            $table->string("telefone", 11)->nullable()->unique();
+            $table->date("data_nascimento")->nullable();
+            $table->string("cidade_nascimento")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
