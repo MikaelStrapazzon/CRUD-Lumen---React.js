e1d5662efb9c77ff98ca1e94500896f787619dcb
Mikael Quadros <mikael.quadros@gmail.com>
2022-03-21 21:23:23 -0300

    Adicionado mais comados do Laravel no Artisan

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -23,9 +23,10 @@ $app = new Laravel\Lumen\Application(
     dirname(__DIR__)
 );
 
-// $app->withFacades();
+$app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
 
-// $app->withEloquent();
+$app->withFacades();
+$app->withEloquent();
 
 /*
 |--------------------------------------------------------------------------
